667e56285c3442c67d0b0ba962743e58e82fa896
Eugenio Lacuesta <eugeniol3@gmail.com>
2014-01-01 22:33:45 -0200

    Added class to allow digits and white spaces. Altered validation messages.

--- js/x2Validate.js
+++ js/x2Validate.js
@@ -11,27 +11,34 @@ $().ready(function() {
 	
 	// ### add validation methods
 	
+	jQuery.validator.addMethod('digits-ws', function(value, element) {
+		return this.optional(element) || /^([0-9]+\s+)*[0-9]+$/i.test(value)
+	}, 'Please enter only digits and whitespaces')
+
 	jQuery.validator.addMethod('lettersonly', function(value, element) {
 		return this.optional(element) || /^[a-z]+$/i.test(value)
-	}, 'Please type only letters')
+	}, 'Please enter only letters')
 	
 	jQuery.validator.addMethod('lettersonly-ws', function(value, element) {
 		return this.optional(element) || /^([a-z]+\s+)*[a-z]+$/i.test(value)
-	}, 'Please type only letters and whitespaces, no special characters')
+	}, 'Please enter only letters and whitespaces, no special characters')
 	
 	jQuery.validator.addMethod('specialchars', function(value, element) {
 		return this.optional(element) || /^[a-zA-Z'ñáéíóúÁÉÍÓÚ]+$/i.test(value)
-	}, 'Please type only letters, no whitespaces')
+	}, 'Please enter only letters, no whitespaces')
 	
 	jQuery.validator.addMethod('specialchars-ws', function(value, element) {
 		return this.optional(element) || /^([a-zA-Z'-áéíóúÁÉÍÓÚ]+\s+)*[a-zA-Z'-áéíóúÁÉÍÓÚ]+$/i.test(value)
-	}, 'Please type only letters and whitespaces')	
+	}, 'Please enter only letters and whitespaces')
 	
 	
 	// ### event handlers for input control
 	
 	// digits
 	$('.digits').on('keypress', function(event){ return inputControl(event, 'digitsonly', false) })
+
+	// digits, allow white space
+	$('.digits-ws').on('keypress', function(event){ return inputControl(event, 'digitsonly', true) })
 	
 	// letters only
 	$('.lettersonly').on('keypress', function(event){ return inputControl(event, 'lettersonly', false) })
